c2d991aa14c46a2648c32086d11641f434685789
zuoyuan <zuoyuan@meituan.com>
2016-07-20 10:04:27 +0800

    add LR model

--- titanic/preprocess_data.py
+++ titanic/preprocess_data.py
@@ -13,12 +13,35 @@ def get_unique_map(s):
         m_dict[v] = i
     return m_dict
 
+def gettitle(x):
+    import re
+    r = re.compile(r'^.+?,(.+\.).+$')
+    m = r.findall(x)
+    if m:
+        return m[0]
+    return ""
+
 def preprocess_data(df, label = True):
-    cols = ["Pclass","Sex","Age","SibSp","Parch","Fare","Embarked"]
+    cols = ["Pclass","Sex","Age","SibSp","Parch","Fare","Cabin","Embarked","Child", "Adult", "Title", "Miss", "Mrs", "Mr", \
+            "FammliySize", "Single", "MidSize", "LargeSize"]
     df2 = df.copy()
+    df2['Child'] = (df.Age < 18).astype(int)
+    df2['Adult'] = (df.Age >=18).astype(int)
+    df2['Title'] = df.Name.map(gettitle)
+    df2['Title'] = df2.Title.map(get_unique_map(df2.Title))
+    df2['Miss'] = df2.Title.map(lambda x: x=='Miss.')
+    df2['Mrs'] = df2.Title.map(lambda x: x=='Mrs.')
+    df2['Mr'] = df2.Title.map(lambda x: x=='Mr.')
+    df2['FammliySize'] = df.SibSp + df.Parch + 1
+    df2['Single'] = df2.FammliySize == 1
+    df2['MidSize'] = (df2.FammliySize > 1) & (df2.FammliySize < 5)
+    df2['LargeSize']= (df2.FammliySize > 4)
+
     df2[['Age','Fare']] = df[['Age','Fare']].fillna(df[['Age','Fare']].mean())
+
     df2['Sex'] = df.Sex.map({'male':1, 'female':0})
     df2['Embarked'] = df.Embarked.map(get_unique_map(df.Embarked))
+    df2['Cabin'] = df.Cabin.fillna('Z').map(lambda x: x[0]).map(get_unique_map(list('ABCDEFGTZ')))
 
     if label:
         return df2[cols], df2.Survived
